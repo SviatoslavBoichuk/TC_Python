b8972844a2809e78e4bf33c0b547861c238ac579
SviatoslavBoichuk <boichuk.sv@gmail.com>
2017-07-03 23:07:02 +0300

    Correct some remarks!

--- TC_Python_task/main.py
+++ TC_Python_task/main.py
@@ -11,13 +11,17 @@ import os
 # 0 if application finished success, otherwise return -1
 EXIT_SUCCESS = 0
 EXIT_FAILURE = -1
+FILE_READ_MODE = "r"
+FILE_WRITE_MODE = "w+"
+FILE_EXTEND = '.txt'
+END_STR = 'end'
+NEW_LINE_SYMBOL = '\n'
 
 # pass file name to main function
 NUM_INPUT_PARAMS = 2
 
 # map that contain addresantss and their packets"""
-contacts = {'Ivan': [], 'Dmythro': [], 'Ostap': [], 'Lesya': []}
-packets = []
+contacts = {'Ivan': [], 'Dmytro': [], 'Ostap': [], 'Lesya': []}
 
 
 # Check if count of input params is less then 2
@@ -34,21 +38,22 @@ def check_exist(
 
 # Read packets from input file
 def get_packets(file_name):
-    global packets
-    with open(file_name, "r") as file_obj:
-        packets = [l.rstrip('\n') for l in file_obj.readlines() if l != '\n']
+    packets = []
+    with open(file_name, FILE_READ_MODE) as file_obj:
+        packets = [packet.rstrip(NEW_LINE_SYMBOL) for packet in file_obj.readlines() if packet != NEW_LINE_SYMBOL]
+    return packets
 
 
 # parse packet to addresantss
 def parse_file(
         packets):
 
-        # if packet is no Ivan, or Dmythro or Lesya, then
+        # if packet is no Ivan, or Dmytro or Lesya, then
         # this packet if for Ostap
         flag = False
 
         for line in packets:
-            if line.split()[-1] == 'end':
+            if line.split()[-1] == END_STR:
                 flag = True
                 contacts['Lesya'].append(line)
             if len(line) % 2 == 0:
@@ -56,7 +61,7 @@ def parse_file(
                 contacts['Ivan'].append(line)
             if len(line) % 2 == 1 and line[0].isupper():
                 flag = True
-                contacts['Dmythro'].append(line)
+                contacts['Dmytro'].append(line)
             if not flag:
                 contacts['Ostap'].append(line)
             flag = False
@@ -64,31 +69,31 @@ def parse_file(
 
 # Write parsed packets to file
 def write(file_name, packet):
-    with open(file_name, "w+") as file_obj:
-        file_obj.writelines([p+'\n' for p in packet])
+    with open(file_name, FILE_WRITE_MODE) as file_obj:
+        file_obj.writelines([(p + NEW_LINE_SYMBOL) for p in packet])
 
 
 def main():
     main_return_code = EXIT_FAILURE
 
     # if user didn't pass
-    # file name to programm, we stop execute
+    # file name to program, we stop execute
     if check_input_params():
         file_name = argv[1]
     else:
-        print("Missing param: [file_name], program close")
+        print("Missing param: [file_name], program close!")
         return EXIT_FAILURE
 
     if check_exist(file_name):
         # read packets from file
-        get_packets(file_name)
+        packets = get_packets(file_name)
         # parse packets
         parse_file(packets)
 
         # write parsed packets to dest. files
         write_files_name = contacts.keys()
         for out_file_name in write_files_name:
-            write(out_file_name+'.txt', contacts[out_file_name])
+            write((out_file_name + FILE_EXTEND), contacts[out_file_name])
         main_return_code = EXIT_SUCCESS
     else:
         main_return_code = EXIT_FAILURE
